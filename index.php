fbc4c3bfec1466ceae6cb93909625bd55a666c15
vabdo <vinicius.abdo.ti@gmail.com>
2018-06-28 01:27:35 -0300

    criado template admim

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
-
+use \Hcode\PageAdmin;
 $app = new Slim();
 
 $app->config('debug', true);
@@ -17,6 +17,16 @@ $app->get('/', function() {
 
 });
 		//roda o destruct -> vai carregar o footer
+
+$app->get('/sistema', function() {
+    
+	$page = new PageAdmin(); //inicia o construct, por padrao vai rodar o header
+
+	$page->setTpl("index"); //carrega o conteudo na pasta view (body)
+
+});
+
+
 $app->run();  //
 
  ?>
