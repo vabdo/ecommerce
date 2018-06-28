bfa273be029b44a4c3e1a7aa4c57ddae78cd7236
vabdo <vinicius.abdo.ti@gmail.com>
2018-06-28 00:34:10 -0300

    criamos a classe Page

--- index.php
+++ index.php
@@ -2,19 +2,21 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql(); 
-	$results = $sql->select("SELECT * FROM tb_users");
-	
-	echo json_encode($results);
+	$page = new Page(); //inicia o construct, por padrao vai rodar o header
+
+	$page->setTpl("index"); //carrega o conteudo na pasta view (body)
 
 });
-
-$app->run();
+		//roda o destruct -> vai carregar o footer
+$app->run();  //
 
  ?>
